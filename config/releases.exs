8c383dbbc71f47da0236ff6c3c7056be8a37da07
ontofractal <v@ontofractal.com>
2020-09-21 20:15:52 +0300

    minor config detail

--- config/releases.exs
+++ config/releases.exs
@@ -7,6 +7,7 @@ config :logger,
   metadata: [:http_method, :http_path, :request_id, :org_id, :user_id]
 
 config :logflare_logger_backend,
+  url: "https://api.logflare.app",
   level: :debug,
   api_key: System.fetch_env!("LOGFLARE_API_KEY"),
   source_id: System.fetch_env!("LOGFLARE_SOURCE_ID"),
